809ad13fc2fd659e700d9dfc8fe6cb3f4b46422e
FelipeElze1010 <felipeelze10@gmail.com>
2026-03-16 22:42:06 -0300

    aula 02 variaveis

---
+++ aula02-vaiaveis-operadores/Arquivo-Exercicios.py
@@ -0,0 +1,17 @@
+#Exercicio 01
+nome = input("Coloque seu nome: ")
+print(f"Seja bem vindo {nome}!")
+
+#Exercicio 02
+
+dia = input("Insira o dia: ")
+mes = input("Insira o mês: ")
+ano = input("Insira o ano: ")
+print(f'Sua data de aniversario é {dia}/{mes}/{ano}')
+
+#Exercicio 03
+
+num1 = int(input("Insira o primeiro numero: "))
+num2 = int(input("Insira o segundo numero: "))
+resultado = num1 + num2
+print(f'O resultado é: {resultado}')
---
+++ aula02-vaiaveis-operadores/aula02-variaveis.py
@@ -0,0 +1,20 @@
+print("Olá Mundo")
+
+print(7 + 4)
+print("7 + 4")
+print("7" + "4")
+
+# VARIAVEIS
+
+nome = "Felipe Elze da Silva"
+idade = int(18)
+tamanho = float(1.86)
+
+# INPUT
+
+Nome = print(input("Digite seu nome: ")) #str
+Idade = print(int(input("Digite sua idade: ")))
+Tamanho = print(float(input("Digite seu tamanho: ")))
+
+#print(f'Suas caracteristicas de cadastro são {Nome, Idade, Tamanho}')
+
